7f201c45e85dcbc874ca1b87fd8598b8d74a0c56
noemi <noemiarismendi.cc@gmail.com>
2017-08-14 09:53:42 -0500

    modificando portafolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,14 @@
 $(".button-collapse").sideNav();
+
+$(document).ready(function(){
+      $('.parallax').parallax();
+    });
+
+$(document).ready(function(){
+      $('.carousel').carousel();
+    });
+
+$(document).ready(function(){
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+  });
